05a1b4afdcec315a14b225c8fec31ad0f76cff02
Elliot Wright <wright.elliot@gmail.com>
2014-07-15 22:35:11 +0100

    Added AccessDeniedException to help application flow.

---
+++ src/Aegis/Exception/AccessDeniedException.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of Aegis.
+ *
+ * (c) Elliot Wright <elliot@elliotwright.co>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Aegis\Exception;
+
+use Aegis\Token\TokenInterface;
+
+/**
+ * Access Denied Exception
+ *
+ * @author Elliot Wright <elliot@elliotwright.co>
+ */
+class AccessDeniedException extends \RuntimeException
+{
+}
